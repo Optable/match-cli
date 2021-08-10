1c2296edfdb266517ab1c955a504c792861c6a24
Justin Li <juan.y.li@mail.mcgill.ca>
2021-08-10 14:08:48 -0400

    wrap connect and listen in go routine to unblock (#4)

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -3,6 +3,7 @@ package network
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"net"
 	"time"
@@ -12,40 +13,47 @@ const network = "tcp"
 
 // Connect establish a tls connection to the endpoint with nagle enabled.
 func Connect(ctx context.Context, endpoint string, cred *tls.Config) (*tls.Conn, error) {
-	raddr, err := net.ResolveTCPAddr(network, endpoint)
-	if err != nil {
-		return nil, fmt.Errorf("failed resolving TCP address of %s: %w", endpoint, err)
+	var conn *tls.Conn
+
+	connectBlock := func() error {
+		for {
+			// dial timeout of 2 seconds.
+			dialConn, err := net.DialTimeout(network, endpoint, 2*time.Second)
+			var netErr net.Error
+			if errors.As(err, &netErr) && netErr.Timeout() {
+				//retry
+				continue
+			}
+
+			if err != nil {
+				return fmt.Errorf("failed to dial %s: %w", endpoint, err)
+			}
+
+			// enable nagle
+			tcpConn := dialConn.(*net.TCPConn)
+			if err := tcpConn.SetNoDelay(false); err != nil {
+				return fmt.Errorf("cannot enable nagle: %w", err)
+			}
+
+			tlsConn := tls.Client(tcpConn, cred)
+			if err := tlsConn.Handshake(); err != nil {
+				time.Sleep(time.Second)
+				continue
+			}
+
+			conn = tlsConn
+			return nil
+		}
 	}
 
-	// timeout after 2 mins
-	timeout := time.NewTimer(2 * time.Minute)
-	for {
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		case <-timeout.C:
-			return nil, fmt.Errorf("connection time exceeded")
-		default:
-			// retry
-		}
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	defer cancel()
 
-		conn, err := net.DialTCP("tcp", nil, raddr)
-		if err != nil {
-			return nil, fmt.Errorf("failed to dial %s: %w", endpoint, err)
-		}
-		// enable nagle
-		if err := conn.SetNoDelay(false); err != nil {
-			return nil, fmt.Errorf("cannot enable nagle: %w", err)
-		}
-
-		c := tls.Client(conn, cred)
-		if err := c.Handshake(); err != nil {
-			time.Sleep(time.Second)
-			continue
-		}
-
-		return c, nil
+	if err := unblock(ctx, connectBlock); err != nil {
+		return nil, err
 	}
+
+	return conn, nil
 }
 
 // Listen listens to a tcp connection to host and returns a nagle enabled tls connection
@@ -55,37 +63,57 @@ func Listen(ctx context.Context, host string, cred *tls.Config) (*tls.Conn, erro
 		return nil, fmt.Errorf("failed resolving TCP address of %s: %w", host, err)
 	}
 
-	timeout := time.NewTimer(2 * time.Minute)
-	for {
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		case <-timeout.C:
-			return nil, fmt.Errorf("connection time exceeded")
-		default:
-			// retry
+	var conn *tls.Conn
+
+	listenBlock := func() error {
+		listen, err := net.ListenTCP(network, laddr)
+		if err != nil {
+			return fmt.Errorf("failed to listen %w", err)
 		}
 
-		l, err := net.ListenTCP(network, laddr)
+		tcpConn, err := listen.AcceptTCP()
 		if err != nil {
-			return nil, fmt.Errorf("failed to listen %w", err)
-		}
-
-		conn, err := l.AcceptTCP()
-		if err != nil {
-			return nil, fmt.Errorf("failed to accept connection: %w", err)
+			return fmt.Errorf("failed to accept connection: %w", err)
 		}
 		// enable nagle
-		if err := conn.SetNoDelay(false); err != nil {
-			return nil, fmt.Errorf("cannot enable nagle: %w", err)
+		if err := tcpConn.SetNoDelay(false); err != nil {
+			return fmt.Errorf("cannot enable nagle: %w", err)
 		}
 
-		c := tls.Server(conn, cred)
-		if err := c.Handshake(); err != nil {
-			time.Sleep(time.Second)
-			continue
+		tlsConn := tls.Server(tcpConn, cred)
+		if err := tlsConn.Handshake(); err != nil {
+			return fmt.Errorf("server: handshake failed: %w", err)
 		}
 
-		return c, nil
+		conn = tlsConn
+		return nil
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	defer cancel()
+
+	if err := unblock(ctx, listenBlock); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func unblock(ctx context.Context, blocking func() error) error {
+	if blocking == nil {
+		return nil
+	}
+
+	err := make(chan error, 1)
+	go func() {
+		defer close(err)
+		err <- blocking()
+	}()
+
+	select {
+	case e := <-err:
+		return e
+	case <-ctx.Done():
+		return ctx.Err()
 	}
 }
