22055291dd6a571a18af4cd4af4f572c3649e446
Justin <justin.li@optable.co>
2021-08-13 11:31:20 -0400

    remove version command

--- pkg/cli/root.go
+++ pkg/cli/root.go
@@ -4,25 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-
-	"github.com/alecthomas/kong"
 )
-
-const VERSION = "v1.0.0"
 
 type Cli struct {
 	Verbose int `opt:"" short:"v" type:"counter" help:"Enable debug mode."`
 
-	Version VersionCmd `cmd:"" help:"Show match-cli version."`
 	Partner PartnerCmd `cmd:"" help:"Partner command."`
 	Match   MatchCmd   `cmd:"" help:"Match command."`
-}
-
-type VersionCmd struct{}
-
-func (v *VersionCmd) Run(ctx *kong.Context) error {
-	ctx.Printf(VERSION)
-	return nil
 }
 
 func (c *Cli) NewContext() (*CliContext, error) {
