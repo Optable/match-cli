ae051e75ef217cfa3b972345cbbeebdfa079985c
Justin Li <justin.li@optable.co>
2021-10-22 11:27:09 -0400

    create config directory with 700 (#14)

--- pkg/cli/config.go
+++ pkg/cli/config.go
@@ -31,7 +31,7 @@ func ensureConfigPath() (string, error) {
 		return "", err
 	}
 	dir := filepath.Dir(path)
-	if err := os.MkdirAll(dir, 0755); err != nil {
+	if err := os.MkdirAll(dir, 0700); err != nil {
 		return "", err
 	}
 	return path, nil
