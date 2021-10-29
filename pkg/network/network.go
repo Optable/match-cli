12d05b9d14e085e48e096f384865e1e432076a14
Justin <justin.li@optable.co>
2021-10-29 12:58:29 -0400

    fix goroutine memory leak

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -3,117 +3,119 @@ package network
 import (
 	"context"
 	"crypto/tls"
-	"errors"
 	"fmt"
 	"net"
 	"time"
 )
 
-const network = "tcp"
+const (
+	matchTCPNetwork = "tcp"
+	connectTimeout  = 6 * time.Minute
+	dialTimeout     = 2 * time.Second
+)
 
-// Connect establish a tls connection to the endpoint with nagle enabled.
+// matchConnect establishes a tls connection to the endpoint with nagle enabled.
 func Connect(ctx context.Context, endpoint string, cred *tls.Config) (*tls.Conn, error) {
-	var conn *tls.Conn
-
-	connectBlock := func() error {
-		for {
-			// dial timeout of 2 seconds.
-			dialConn, err := net.DialTimeout(network, endpoint, 2*time.Second)
-			var netErr net.Error
-			if errors.As(err, &netErr) && netErr.Timeout() {
-				//retry
-				continue
-			}
-
-			if err != nil {
-				return fmt.Errorf("failed to dial %s: %w", endpoint, err)
-			}
-
-			// enable nagle
-			tcpConn := dialConn.(*net.TCPConn)
-			if err := tcpConn.SetNoDelay(false); err != nil {
-				return fmt.Errorf("cannot enable nagle: %w", err)
-			}
-
-			tlsConn := tls.Client(tcpConn, cred)
-			if err := tlsConn.Handshake(); err != nil {
-				time.Sleep(time.Second)
-				continue
-			}
-
-			conn = tlsConn
-			return nil
-		}
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	// timeout for PSI pod to pod connection
+	ctx, cancel := context.WithTimeout(ctx, connectTimeout)
 	defer cancel()
 
-	if err := unblock(ctx, connectBlock); err != nil {
-		return nil, err
+	// tryConnect returns a boolean to signal that we are done retrying connecting to
+	// the listener, as well as the obtained TLS conn, and any errors that results from
+	// connecting.
+	tryConnect := func(ctx context.Context) (bool, *tls.Conn, error) {
+		dialCtx, dialCancel := context.WithTimeout(ctx, dialTimeout)
+		defer dialCancel()
+
+		var dialer net.Dialer
+		// DialContext returns an error if the context's timeout is reached.
+		// this makes sure that matchConnect would not loop forever to retry
+		// connections
+		dialConn, err := dialer.DialContext(dialCtx, matchTCPNetwork, endpoint)
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				// if main context is timed out, abort operation
+				return true, nil, fmt.Errorf("dial PSI timed out: %w", ctx.Err())
+			default:
+				//retry on any dial errors
+				return false, nil, nil
+			}
+		}
+
+		// Disable TCP_NODELAY enables nagle's algorithm
+		// which collects small packets and send them once
+		// instead of sending each packet as soon as they are available.
+		tcpConn := dialConn.(*net.TCPConn)
+		if err = tcpConn.SetNoDelay(false); err != nil {
+			dialConn.Close()
+			return true, nil, fmt.Errorf("failed to enable nagle: %w", err)
+		}
+
+		tlsConn := tls.Client(tcpConn, cred)
+		// test the tls connection
+		if err = tlsConn.Handshake(); err != nil {
+			dialConn.Close()
+			return true, nil, fmt.Errorf("client: failed to establish a secure tls connection: %w", err)
+		}
+
+		// succeed in establishing a tls connection
+		return true, tlsConn, nil
 	}
 
-	return conn, nil
+	for {
+		done, tlsConn, err := tryConnect(ctx)
+		if !done {
+			// retry connection
+			continue
+		}
+
+		return tlsConn, err
+	}
 }
 
-// Listen listens to a tcp connection to host and returns a nagle enabled tls connection
+// matchListen listens to a host and returns a nagle enabled tls connection
 func Listen(ctx context.Context, host string, cred *tls.Config) (*tls.Conn, error) {
-	laddr, err := net.ResolveTCPAddr(network, host)
-	if err != nil {
-		return nil, fmt.Errorf("failed resolving TCP address of %s: %w", host, err)
-	}
-
-	var conn *tls.Conn
-
-	listenBlock := func() error {
-		listen, err := net.ListenTCP(network, laddr)
-		if err != nil {
-			return fmt.Errorf("failed to listen %w", err)
-		}
-
-		tcpConn, err := listen.AcceptTCP()
-		if err != nil {
-			return fmt.Errorf("failed to accept connection: %w", err)
-		}
-		// enable nagle
-		if err := tcpConn.SetNoDelay(false); err != nil {
-			return fmt.Errorf("cannot enable nagle: %w", err)
-		}
-
-		tlsConn := tls.Server(tcpConn, cred)
-		if err := tlsConn.Handshake(); err != nil {
-			return fmt.Errorf("server: handshake failed: %w", err)
-		}
-
-		conn = tlsConn
-		return nil
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	// timeout for PSI pod to pod connection
+	ctx, cancel := context.WithTimeout(ctx, connectTimeout)
 	defer cancel()
 
-	if err := unblock(ctx, listenBlock); err != nil {
-		return nil, err
+	var lc net.ListenConfig
+	listener, err := lc.Listen(ctx, matchTCPNetwork, host)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen %w", err)
 	}
 
-	return conn, nil
-}
-
-func unblock(ctx context.Context, blocking func() error) error {
-	if blocking == nil {
-		return nil
-	}
-
-	err := make(chan error, 1)
+	// unblock Accept when context deadline is reached.
 	go func() {
-		defer close(err)
-		err <- blocking()
+		<-ctx.Done()
+		// closing the listener will unblock Accept and make Accept return an error
+		// since the context will always timeout
+		// the listener will be closed as soon as either connectTimeout is reached
+		// or the parent context's timeout is reached.
+		listener.Close()
 	}()
 
-	select {
-	case e := <-err:
-		return e
-	case <-ctx.Done():
-		return ctx.Err()
+	conn, err := listener.Accept()
+	if err != nil {
+		return nil, fmt.Errorf("failed to accept connection: %w", err)
 	}
+
+	// Disable TCP_NODELAY enables nagle's algorithm
+	// which collects small packets and send them once
+	// instead of sending each packet as soon as they are available.
+	tcpConn := conn.(*net.TCPConn)
+	if err := tcpConn.SetNoDelay(false); err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("failed to enable nagle: %w", err)
+	}
+
+	tlsConn := tls.Server(tcpConn, cred)
+	// test the tls connection
+	if err := tlsConn.Handshake(); err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("server: failed to establish a secure tls connection: %w", err)
+	}
+
+	return tlsConn, nil
 }
