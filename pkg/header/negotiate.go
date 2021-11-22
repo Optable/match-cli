d84eb65d368d25a0570f1a47cd132b8807512ebb
Xavier Capaldi <xavier.capaldi@optable.co>
2021-11-22 09:33:44 -0500

    Receiver will send the final protocol whether sender's proposal was accepted or not

--- pkg/header/negotiate.go
+++ pkg/header/negotiate.go
@@ -8,21 +8,16 @@ import (
 )
 
 // NegotiateSenderProtocol sends the desired protocol to the receiver.
-// The receiver responds whether the protocol is accepted or not.
-// If the protocol is not accepted, fall back to DHPSI.
+// The receiver responds with the same protocol if it is accepted.
+// If the protocol is not accepted, the receiver will send back the default protocol.
 func NegotiateSenderProtocol(rw io.ReadWriter, protocol psi.Protocol) (psi.Protocol, error) {
 	if n, err := rw.Write([]uint8{uint8(protocol)}); err != nil || n != 1 {
 		return protocol, fmt.Errorf("failed to send protocol negotiation message: %w", err)
 	}
-	ack := make([]byte, 1)
-	if _, err := rw.Read(ack); err != nil {
-		return protocol, fmt.Errorf("sender failed to receive PSI protocol negotiation message, got: %v, err: %w", protocol, err)
+	recProtocol := make([]byte, 1)
+	if _, err := rw.Read(recProtocol); err != nil {
+		return protocol, fmt.Errorf("failed to receive PSI protocol negotiation message, got: %v, err: %w", protocol, err)
 	}
 
-	// Receiver doesn't accept proposed PSI so fall back on DHPSI
-	if ack[0] == 0 {
-		return psi.ProtocolDHPSI, nil
-	}
-
-	return protocol, nil
+	return psi.Protocol(recProtocol[0]), nil
 }
