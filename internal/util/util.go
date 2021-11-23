c360868f5fff33f17e22ea4960ebd340fc41e4ca
Justin <justin.li@optable.co>
2021-11-23 11:37:41 -0500

    add clamping logic to match-cli

--- internal/util/util.go
+++ internal/util/util.go
@@ -5,15 +5,17 @@ import (
 	"context"
 	"io"
 	"os"
+	"strings"
 
+	v1 "github.com/optable/match-api/match/v1"
 	"github.com/rs/zerolog"
 )
 
 //GetInputChannel reads identifiers from a file to a channel
-func GenInputChannel(ctx context.Context, f *os.File) (int64, <-chan []byte, error) {
-	n, err := count(f)
+func GenInputChannel(ctx context.Context, f *os.File) (int64, *v1.Insights, <-chan []byte, error) {
+	n, insight, err := count(f)
 	if err != nil {
-		return n, nil, err
+		return n, nil, nil, err
 	}
 
 	// rewind
@@ -42,22 +44,46 @@ func GenInputChannel(ctx context.Context, f *os.File) (int64, <-chan []byte, err
 		}
 	}()
 
-	return n, identifiers, nil
+	return n, insight, identifiers, nil
 }
 
-//count returns number of lines in file
-func count(r io.Reader) (int64, error) {
+// count returns number of lines in file, as well as the
+// number of each id type
+func count(r io.Reader) (int64, *v1.Insights, error) {
 	var n int64
+	var insight v1.Insights
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
+		s := string(scanner.Bytes())
+		switch {
+		case strings.HasPrefix(s, "e:"):
+			insight.Emails++
+		case strings.HasPrefix(s, "p:"):
+			insight.PhoneNumbers++
+		case strings.HasPrefix(s, "i4:"):
+			insight.Ipv4S++
+		case strings.HasPrefix(s, "i6:"):
+			insight.Ipv6S++
+		case strings.HasPrefix(s, "a:"):
+			insight.AppleIdfas++
+		case strings.HasPrefix(s, "g:"):
+			insight.GoogleGaids++
+		case strings.HasPrefix(s, "r:"):
+			insight.RokuRidas++
+		case strings.HasPrefix(s, "s:"):
+			insight.SamsungTifas++
+		case strings.HasPrefix(s, "f:"):
+			insight.AmazonAfais++
+		}
+		//TODO: do we handle invalid prefix type?
 		n++
 	}
 
 	if err := scanner.Err(); err != nil {
-		return n, err
+		return n, nil, err
 	}
 
-	return n, nil
+	return n, &insight, nil
 }
 
 // safeReadLine reads each line until a newline character and returns
@@ -70,4 +96,42 @@ func safeReadLine(r *bufio.Reader) (line []byte, err error) {
 		line = line[:len(line)-1]
 	}
 	return
+}
+
+// clamp changes the received numbers from the partner which can have differential privacy noise in them,
+// meaning the numbers could be negative or exceed the total number of IDs.
+// We want to normalize the number to be 0 <= candidate <= maxValue
+func clamp(max, n int64) int64 {
+	if n < 0 {
+		return 0
+	}
+	if n > max {
+		return max
+	}
+	return n
+}
+
+func ClampMatchResult(result *v1.ExternalMatchResult, srcInsight *v1.Insights) {
+	for idkind := range v1.IdKind_name {
+		switch v1.IdKind(idkind) {
+		case v1.IdKind_ID_KIND_EMAIL_HASH:
+			result.Insights.Emails = clamp(srcInsight.Emails, result.Insights.Emails)
+		case v1.IdKind_ID_KIND_PHONE_NUMBER:
+			result.Insights.PhoneNumbers = clamp(srcInsight.PhoneNumbers, result.Insights.PhoneNumbers)
+		case v1.IdKind_ID_KIND_IPV4:
+			result.Insights.Ipv4S = clamp(srcInsight.Ipv4S, result.Insights.Ipv4S)
+		case v1.IdKind_ID_KIND_IPV6:
+			result.Insights.Ipv6S = clamp(srcInsight.Ipv6S, result.Insights.Ipv6S)
+		case v1.IdKind_ID_KIND_APPLE_IDFA:
+			result.Insights.AppleIdfas = clamp(srcInsight.AppleIdfas, result.Insights.AppleIdfas)
+		case v1.IdKind_ID_KIND_GOOGLE_GAID:
+			result.Insights.GoogleGaids = clamp(srcInsight.GoogleGaids, result.Insights.GoogleGaids)
+		case v1.IdKind_ID_KIND_ROKU_RIDA:
+			result.Insights.RokuRidas = clamp(srcInsight.RokuRidas, result.Insights.RokuRidas)
+		case v1.IdKind_ID_KIND_SAMSUNG_TIFA:
+			result.Insights.SamsungTifas = clamp(srcInsight.SamsungTifas, result.Insights.SamsungTifas)
+		case v1.IdKind_ID_KIND_AMAZON_AFAI:
+			result.Insights.AmazonAfais = clamp(srcInsight.AmazonAfais, result.Insights.AmazonAfais)
+		}
+	}
 }
